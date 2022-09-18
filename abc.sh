2949472b32a6a494707f4a3ab43119ddc6fd78f4
Kishore Kamatham <kkamatham@roku.com>
2022-09-18 09:52:16 +0100

    kishore commits

---
+++ abc.sh
@@ -0,0 +1,3 @@
+# /bin/bash
+echo "this file ios created by kishore"
+eco "this file has merig conflicts"
--- revanth.sh
+++ revanth.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "this file is created by revanth"
+echo "This file is update by kishore"
